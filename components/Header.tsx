3f0e4adce6b5ea48540e31ce53002c111884b2ea
”Riazul-dev” <riazul4u@gmail.com>
2024-12-27 23:10:13 +0600

    edit file

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="flex justify-between items-center py-4">
       <div>
-        <Link href={"/"}>Logo</Link>
+        <Link href="/">Logo</Link>
       </div>
       <div>
         <input className="shadow-sm rounded-full bg-slate-100 px-4 py-1" type="text" placeholder="Type..." />
